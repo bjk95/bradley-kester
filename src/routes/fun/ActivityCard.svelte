b6f3c0bffe6593d215f37533ee577cb893124508
bjk95 <bradley.kester@barcelonagse.eu>
2023-08-22 11:36:16 +1000

    style(ActivityCard.svelte): change layout from flexbox to grid for better responsiveness style(ActivityCard.svelte): adjust image size on larger screens to prevent overflow

--- src/routes/fun/ActivityCard.svelte
+++ src/routes/fun/ActivityCard.svelte
@@ -7,8 +7,8 @@
     export let imagePath: string
 </script>
 
-<section class={twMerge("flex bg-gray-800 rounded-md", reverse ? 'flex-row-reverse' : '')}>
-	<img class=" max-h-96 object-contain" src={imagePath} alt="Quantexa UI"  />
+<section class={twMerge("grid grid-cols-1 lg:grid-cols-2 bg-gray-800 rounded-md", reverse ? 'flex-row-reverse' : '')}>
+	<img class="lg:max-h-96 object-contain" src={imagePath} alt="Quantexa UI"  />
     <div class=" p-8">
         <h2 class="text-3xl font-semibold">{name}</h2>
         <div class="h-4" />
